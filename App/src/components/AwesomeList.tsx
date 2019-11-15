916db538379bacee786aa47a182732abf4be7971
Quini Macbook <quini.emancipia@gmail.com>
2019-11-15 23:11:35 +0100

    Create AwesomeList.tsx

---
+++ App/src/components/AwesomeList.tsx
@@ -0,0 +1,21 @@
+import { IonButtons, IonContent, IonHeader, IonIcon, IonItem, IonList, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+
+const AwesomeList: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>List</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <ListItems />
+      </IonContent>
+    </IonPage>
+  );
+};
