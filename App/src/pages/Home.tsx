a2a3009cf60b643dde8e45dc2d7b2035ed32e4b0
Clare <claretgb@gmail.com>
2020-04-12 14:21:25 +0200

    just in case i fuck it up

--- App/src/pages/Home.tsx
+++ App/src/pages/Home.tsx
@@ -28,37 +28,8 @@ import './Home.css';
 const HomePage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons>
-            <IonMenuButton />
-            <IonGrid>
-              <IonRow align-items-right>
-                <IonCol>
-                  <h1>Ceres</h1>
-                </IonCol>
-                <IonCol size = '1'>
-                  <IonImg src = "/img/logo.svg" alt = ''></IonImg>
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonButtons>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonCard className="welcome-card">
-          <img src="/assets/icon/lesvaques.jpg" alt=""/>
-          <IonCardHeader>
-            <IonCardSubtitle>Local y sostenible.</IonCardSubtitle>
-            <IonCardTitle>¡Bienvenido a Ceres!</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <p>
-              Desde Ceres queremos ayudarte a encontrar los mejores productos orgánicos y artesanos, a la venta a tu alrededor. También queremos darte ideas de actividades que puedes hacer todos los días, libres de contaminación, y acercar a los consumidores a los mercados de su comunidad, para fomentar el desarrollo de los negocios de la zona.
-            </p>
-          </IonCardContent>
-        </IonCard>
-
+        <img src="/assets/icon/muve.jpeg" alt=""/>
       </IonContent>
     </IonPage>
   );
